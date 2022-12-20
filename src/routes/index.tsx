6e4bb48c1ece78182356d0c3a3cce3a0bc753318
Joao Mateus <joao.amaral@ifam.edu.br>
2022-12-19 23:04:11 -0300

    cleaning files

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import { Home } from '../pages/Home'
 import { SignIn } from '../pages/Sign-in'
 import { SignUp } from '../pages/Sign-up'
