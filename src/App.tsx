2e859fc35bc1622616470f1906658cf95add7d0e
Joao Mateus <joao.amaral@ifam.edu.br>
2022-12-26 13:27:09 -0300

    feat: theme switcher (avoid rendering on app with window.location just while auth is not implemented)

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,23 @@ import { useState } from 'react'
 import { AppRoutes } from './routes'
 import { Header } from './components/Header'
 import GlobalStyle from './styles/global';
-import light from './styles/themes/light'
+import light from './styles/themes/light';
+import dark from './styles/themes/dark';
 import { ThemeProvider } from 'styled-components';
 
 export const App: React.FC = () => {
+  const [theme, setTheme] = useState(light)
+
+  const toggleTheme = () => {
+    setTheme(theme.title === 'light' ? dark : light);
+  }
+
   return (
-    <ThemeProvider theme={light}>
+    <ThemeProvider theme={theme}>
       <div className="App">
-        <GlobalStyle/>
+        <GlobalStyle />
         <AppRoutes />
-        <Header />
+        {window.location.pathname !== '/sign_in' && window.location.pathname !== '/sign_up' ? <Header toggleTheme={toggleTheme} /> : null}
       </div>
     </ThemeProvider>
   )
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,12 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import { Home } from '../pages/Home'
 import { SignIn } from '../pages/Sign-in'
 import { SignUp } from '../pages/Sign-up'
+import { Header } from '../components/Header'
+import { render } from 'react-dom'
 
-export const AppRoutes = () => {
+export const AppRoutes: React.FC = () => {
+
    return(
     <Routes>
         <Route path='/' element={<Home />} />
